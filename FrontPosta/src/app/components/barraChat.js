48d12a024aa4fe05dcae5e7e4a881806a29864e2
milogomez1 <163532183+milogomez1@users.noreply.github.com>
2024-11-08 12:45:29 +0100

    milo 11.8.8

--- FrontPosta/src/app/components/barraChat.js
+++ FrontPosta/src/app/components/barraChat.js
@@ -3,7 +3,7 @@ import styles from "./barraChat.module.css" ;
 
 const BarraChat = ({}) => {
     return(
-        <input className={styles.input} type="text" placeholder="Mensaje " id ="barraChat" />
+        <input className={styles.input} type="text" placeholder="Mensajes " id ="barraChat" />
     )
 }
 
